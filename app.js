ba1fe3337a30f3d2faeeaf64a36fa0c92a75107f
Muhammadali Rahimov <Muhammadali.Rahimov@msg.group>
2024-11-07 10:36:14 +0100

    arrays

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("asdasda")
+const arr = ['!', 'JS', 'love', 'I'];
+const newArr = [];
+
+for(i = arr.length - 1; i >= 0; i = i - 1) {  
+    newArr.push(arr[i]);
+}
+console.log(newArr.join(' '));
+
