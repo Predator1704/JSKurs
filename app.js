974bf6a770f364fe59d796e648a05bc2431b4568
Muhammadali Rahimov <Muhammadali.Rahimov@msg.group>
2024-11-07 15:26:26 +0100

    Operations counter (Arrays and for)

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-const arr = ['!', 'JS', 'love', 'I'];
-const newArr = [];
+const operations = [1000, -700, 300, -500, 10000];
+const myBalance = 100;
 
-for(i = arr.length - 1; i >= 0; i = i - 1) {  
-    newArr.push(arr[i]);
+function Summ(Array = [], myBalance = 0) {
+    let sum = 0;
+    for (let i = 0; i < Array.length; i++) {
+        sum += Array[i]
+        if (sum < 0) {
+            return false;
+        }
+    }
+    return myBalance + sum;
 }
-console.log(newArr.join(' '));
+
+console.log(`
+    your current Balance:
+    ${Summ(operations, myBalance).toFixed(2)}$
+    `);
+
+const income = [];
+const expense = [];
+let averageIncome;
+let averageExpense;
+
+function average() {
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i] < 0) {
+            continue;
+        } 
+        income.push(operations[i]);
+        averageIncome = Summ(income) / income.length;
+    }
+        for (let i = 0; i < operations.length; i++) {
+        if (operations[i] > 0) {
+            continue;
+        } 
+        expense.push(operations[i]);
+        averageExpense = expense.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / expense.length;
+    }
+        return (`your average income: ${averageIncome.toFixed(2)}$ and your average expence ${averageExpense.toFixed(2)}$ `);
+}
+console.log(average());
+
+
 
